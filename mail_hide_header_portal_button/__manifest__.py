c4e0d1e5d66f17c568ed661d985d406f74a513ad
manu <manuel.regidor@sygel.es>
2024-07-03 15:05:03 +0200

    mail_hide_body_portal_button, mail_hide_header_portal_button: hide links to portal in mails

---
+++ mail_hide_header_portal_button/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+{
+    "name": "Mail Hide Header Portal Button",
+    "version": "15.0.1.0.0",
+    "category": "Mail",
+    "summary": "Hide header button to portal in emails",
+    "author": "Sygel, Odoo Community Association (OCA)",
+    "website": "https://github.com/sygel-technology/sy-social",
+    "license": "AGPL-3",
+    "depends": ["mail"],
+    "installable": True,
+}
---
+++ mail_hide_header_portal_button/models/mail_thread.py
@@ -0,0 +1,19 @@
+# Copyright 2024 Manuel Regidor <manuel.regidor@sygel.es>
+# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
+
+from odoo import models
+
+
+class MailThread(models.AbstractModel):
+    _inherit = "mail.thread"
+
+    def _notify_classify_recipients(self, recipient_data, model_name, msg_vals=None):
+        result = super()._notify_classify_recipients(
+            recipient_data,
+            model_name,
+            msg_vals
+        )
+        for group in result:
+            if group.get("notification_group_name") not in ['customer', 'portal']:
+                group['has_button_access'] = False
+        return result
