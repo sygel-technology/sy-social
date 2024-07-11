6bbfeb154d5d37806d2e990f9e7960287235c911
Manuel Florido <manuel.florido@sygel.es>
2024-07-11 12:09:29 +0200

    [T-5944][IMP]disabled_mail_follower_invitation,mail_hide_body_portal_button,mail_hide_header_portal_button,mail_show_follower,message_connector_base,message_connector_slack:pre-commit auto fixes

--- mail_show_follower/models/res_users.py
+++ mail_show_follower/models/res_users.py
@@ -1,13 +1,10 @@
 # Copyright 2020 Valentin Vinagre <valentin.vinagre@sygel.es>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
 
-from odoo import models, fields
+from odoo import fields, models
 
 
 class ResUser(models.Model):
     _inherit = "res.users"
 
-    show_in_cc = fields.Boolean(
-        string='Show in CC',
-        default=True
-    )
+    show_in_cc = fields.Boolean(string="Show in CC", default=True)
