10c0958d7f69fc6d8849e24f4077b523d3ef4438
manu <manuel.regidor@sygel.es>
2024-09-13 12:29:15 +0200

    [T-6565][MIG] mail_show_follower: migration to 16.0

--- mail_show_follower/__manifest__.py
+++ mail_show_follower/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Mail Show Follower",
     "summary": "Show CC document followers in mails.",
-    "version": "15.0.1.0.1",
+    "version": "16.0.1.0.0",
     "category": "Mail",
     "website": "https://github.com/sygel-technology/sy-social",
     "author": "Sygel, Odoo Community Association (OCA)",
